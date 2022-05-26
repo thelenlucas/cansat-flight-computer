63b55a2df78a99ab1d582f4343d7792ad7830250
thelenlucas <89479728+thelenlucas@users.noreply.github.com>
2022-05-26 18:48:12 -0500

    Updated for use in datalogging

--- groundstation.py
+++ groundstation.py
@@ -1,16 +1,19 @@
-import serial
-ser = serial.Serial('COM7', 9600)
+import serial, csv
+ser = serial.Serial('COM5', 115200)
 ser.write("go".encode("utf-8"))
 
 
 data = []
 while True:
-    curr = str(ser.readline()) + "\n"
-    curr = curr[2:-6]
-    data.append(curr)
-    print(curr)
-    a = open("logs.csv", "a")
-
-    a.write(curr)
-    a.close()
+    curr = str(ser.readline())
     
+    curr = curr[2:-6]
+    ls = curr.split(",")
+
+    data.append(ls)
+    print(ls)
+
+    with open('temperature_logs.csv', 'a', newline="") as f:
+      
+        # using csv.writer method from CSV package
+        f.write(curr + "\n")
